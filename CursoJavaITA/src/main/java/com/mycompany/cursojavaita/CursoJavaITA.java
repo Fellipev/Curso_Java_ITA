5211efe355740287cf868c9a4273a36b95dd286c
Fellipev <fellipe2reis@gmail.com>
2024-01-20 00:37:43 -0300

    criacao do projeto

---
+++ CursoJavaITA/src/main/java/com/mycompany/cursojavaita/CursoJavaITA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cursojavaita;
+
+/**
+ *
+ * @author Fellipe
+ */
+public class CursoJavaITA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
